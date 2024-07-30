7f204e839c8ec61f96b6cf7d647cf117a6b3c147
jnthnexpdev <jnthnexpdev@gmail.com>
2024-07-30 16:00:24 -0600

    Add endpoint for add items to order

--- controllers/order/order_controller.js
+++ controllers/order/order_controller.js
@@ -10,12 +10,11 @@ const registerOrder = async (req, res) => {
     try {
         const { id_franquicia, folio, nombre_cliente, tipo_orden, estatus, mesa, mesero } = req.body;
         const prod = req.body.productos;
-        const comb = req.body.combos;
+        const comb = req.body.combos; 
         const { fecha, hora } = await getDateAndTime();
 
         let total = new Decimal(0);
 
-        // Calcular subtotales de productos
         const updatedProductos = await Promise.all(prod.map(async (product) => {
             const menu = await menuModel.findById(product.id_menu);
             if (!menu) {
@@ -925,6 +924,247 @@ const changeOrderStatus = async(req, res) => {
     }
 };  
 
+//Agregar productos a una orden
+const addProductsToOrder = async(req, res) => {
+    try{
+        const idOrder = req.params.id;
+        const prod = req.body.productos;
+        const comb = req.body.combos; 
+        const isValidId = isValidObjectId(idOrder);
+        if (!isValidId) {
+            return res.status(400).json({
+                success: false,
+                httpCode: 400,
+                message: 'Id orden requerido'
+            });
+        }
+
+        const order = await orderModel.findById(idOrder);
+        if (!order) {
+            return res.status(404).json({
+                success: false,
+                httpCode: 404,
+                message: 'La orden no existe'
+            });
+        }
+
+        // Validar y agregar productos
+        let total = new Decimal(order.total);
+        let productsList = [...order.productos];
+        let combosList = [...order.combos];
+
+        if (prod && prod.length > 0) {
+            const updatedProductos = await Promise.all(prod.map(async (product) => {
+                const menu = await menuModel.findById(product.id_menu);
+                if (!menu) {
+                    return res.status(404).json({
+                        success: false,
+                        message: `Id del menu no encontrado`,
+                    });
+                }
+                const productDetails = menu.productos.id(product.id_producto);
+                if (productDetails) {
+                    let subtotal = new Decimal(productDetails.costo).times(product.cantidad);
+
+                    // Agregar costo de ingredientes extra
+                    if (product.ingredientes_extra && product.ingredientes_extra.length > 0) {
+                        for (const extra of product.ingredientes_extra) {
+                            const extraCost = new Decimal(extra.costo).times(product.cantidad);
+                            subtotal = subtotal.plus(extraCost);
+                        }
+                    }
+
+                    total = total.plus(subtotal);
+
+                    return {
+                        ...product,
+                        nombre: productDetails.nombre,
+                        pesaje: productDetails.pesaje,
+                        categoria: productDetails.categoria,
+                        descripcion: productDetails.descripcion,
+                        costo: productDetails.costo,
+                        estatus: productDetails.estatus,
+                        subtotal: subtotal.toFixed(2),
+                    };
+                }
+            }));
+
+            productsList = [...productsList, ...updatedProductos];
+        }
+
+        // Validar y agregar combos
+        if (comb && comb.length > 0) {
+            const updatedCombos = await Promise.all(comb.map(async (combo) => {
+                const menu = await menuModel.findById(combo.id_menu);
+                if (!menu) {
+                    return res.status(404).json({
+                        success: false,
+                        message: `Id del menu no encontrado`,
+                    });
+                }
+                const comboDetails = menu.combos.id(combo.id_combo);
+                if (comboDetails) {
+                    let subtotal = new Decimal(comboDetails.costo).times(combo.cantidad);
+
+                    // Agregar costo de ingredientes extra
+                    if (combo.ingredientes_extra && combo.ingredientes_extra.length > 0) {
+                        for (const extra of combo.ingredientes_extra) {
+                            const extraCost = new Decimal(extra.costo).times(combo.cantidad);
+                            subtotal = subtotal.plus(extraCost);
+                        }
+                    }
+
+                    total = total.plus(subtotal);
+
+                    return {
+                        ...combo,
+                        nombre: comboDetails.nombre,
+                        descripcion: comboDetails.descripcion,
+                        costo: comboDetails.costo,
+                        estatus: comboDetails.estatus,
+                        subtotal: subtotal.toFixed(2),
+                    };
+                }
+            }));
+
+            combosList = [...combosList, ...updatedCombos];
+        }
+
+        // Calcula el IVA y el total
+        const iva = total.times(0.16).toFixed(2);
+        const subtotal = total.minus(new Decimal(iva)).toFixed(2);
+
+        // Actualizar la orden en la base de datos
+        order.productos = productsList;
+        order.combos = combosList;
+        order.subtotal = parseFloat(subtotal);
+        order.iva = parseFloat(iva);
+        order.total = parseFloat(total);
+
+        await order.save();
+
+        return res.status(200).json({
+            success: true,
+            httpCode: 200,
+            message: 'Productos y combos agregados a la orden',
+            order,
+        });
+    }catch(error){
+        handle(res, error);
+    }
+};
+
+//Eliminar productos a una orden
+const removeProductsToOrder = async(req, res) => {
+    try{
+        const idOrder = req.params.id;
+        const { productos, combos } = req.body;
+
+        const isValidId = isValidObjectId(idOrder);
+        if (!isValidId) {
+            return res.status(400).json({
+                success: false,
+                httpCode: 400,
+                message: 'Id orden requerido'
+            });
+        }
+
+        const order = await orderModel.findById(idOrder);
+        if (!order) {
+            return res.status(404).json({
+                success: false,
+                httpCode: 404,
+                message: 'La orden no existe'
+            });
+        }
+
+        // Actualizar productos
+        if (productos && productos.length > 0) {
+            for (const prod of productos) {
+                const orderProduct = order.productos.find(p => p.id_producto.toString() === prod.id_producto);
+                if (orderProduct) {
+                    if (prod.cantidad >= orderProduct.cantidad) {
+                        order.productos.pull({ _id: orderProduct._id });
+                        console.log(`Producto eliminado: ${orderProduct._id}`);
+                    } else {
+                        orderProduct.cantidad -= prod.cantidad;
+                        let subtotal = new Decimal(orderProduct.costo).times(orderProduct.cantidad);
+
+                        // Actualizar costo de ingredientes extra
+                        if (orderProduct.ingredientes_extra && orderProduct.ingredientes_extra.length > 0) {
+                            for (const extra of orderProduct.ingredientes_extra) {
+                                const extraCost = new Decimal(extra.costo).times(orderProduct.cantidad);
+                                subtotal = subtotal.plus(extraCost);
+                            }
+                        }
+
+                        orderProduct.subtotal = subtotal.toFixed(2);
+                        console.log(`Producto actualizado: ${orderProduct._id} - Nueva cantidad: ${orderProduct.cantidad} - Nuevo subtotal: ${orderProduct.subtotal}`);
+                    }
+                } else {
+                    console.log(`Producto no encontrado: ${prod.id_producto}`);
+                }
+            }
+        }
+
+        // Actualizar combos
+        if (combos && combos.length > 0) {
+            for (const comb of combos) {
+                const orderCombo = order.combos.find(c => c.id_combo.toString() === comb.id_combo);
+                if (orderCombo) {
+                    if (comb.cantidad >= orderCombo.cantidad) {
+                        order.combos.pull({ _id: orderCombo._id });
+                        console.log(`Combo eliminado: ${orderCombo._id}`);
+                    } else {
+                        orderCombo.cantidad -= comb.cantidad;
+                        let subtotal = new Decimal(orderCombo.costo).times(orderCombo.cantidad);
+
+                        // Actualizar costo de ingredientes extra
+                        if (orderCombo.ingredientes_extra && orderCombo.ingredientes_extra.length > 0) {
+                            for (const extra of orderCombo.ingredientes_extra) {
+                                const extraCost = new Decimal(extra.costo).times(orderCombo.cantidad);
+                                subtotal = subtotal.plus(extraCost);
+                            }
+                        }
+
+                        orderCombo.subtotal = subtotal.toFixed(2);
+                        console.log(`Combo actualizado: ${orderCombo._id} - Nueva cantidad: ${orderCombo.cantidad} - Nuevo subtotal: ${orderCombo.subtotal}`);
+                    }
+                } else {
+                    console.log(`Combo no encontrado: ${comb.id_combo}`);
+                }
+            }
+        }
+
+        // Recalcular subtotal, IVA y total
+        let newSubtotal = new Decimal(0);
+        order.productos.forEach(product => {
+            newSubtotal = newSubtotal.plus(product.subtotal);
+        });
+        order.combos.forEach(combo => {
+            newSubtotal = newSubtotal.plus(combo.subtotal);
+        });
+
+        const newIva = newSubtotal.times(0.16).toFixed(2);
+        const newTotal = newSubtotal.plus(newIva).toFixed(2);
+
+        order.subtotal = parseFloat(newSubtotal.toFixed(2));
+        order.iva = parseFloat(newIva);
+        order.total = parseFloat(newTotal);
+
+        await order.save();
+
+        return res.status(200).json({
+            success: true,
+            httpCode: 200,
+            message: 'Items eliminados y orden actualizada',
+            order
+        });
+    }catch(error){
+        handle(res, error);
+    }
+};
+
 //Eliminar orden por id
 const deleteOrder = async(req, res) => {
     try{
@@ -1009,6 +1249,8 @@ module.exports = {
     completedOrdersAtHome,
     completedOrdersByTable,
     changeOrderStatus,
+    addProductsToOrder,
+    removeProductsToOrder,
     deleteAllOrdersByFranchise,
     deleteOrder,
     deleteAllOrders
--- routes/order/order_routes.js
+++ routes/order/order_routes.js
@@ -2,6 +2,8 @@ const controller = require('../../controllers/order/order_controller');
 const express = require('express');
 const router = express.Router();
 
+// :id -> order id
+
 router.post('/register', controller.registerOrder);
 router.get('/search-order/:id', controller.searchOrder);
 router.get('/all-orders-franchise/:franchise', controller.allOrdersByFranchise);
@@ -13,6 +15,8 @@ router.get('/back-orders-restaurant/:franchise', controller.backOrdersRestaurant
 router.get('/completed-orders-restaurant/:franchise', controller.completedOrdersRestaurant);
 router.get('/completed-orders-at-home/:franchise', controller.completedOrdersAtHome);
 router.patch('/complete-order/:id', controller.changeOrderStatus);
+router.put('/add-products-to-order/:id', controller.addProductsToOrder);
+router.put('/remove-products-to-order/:id', controller.removeProductsToOrder);
 router.delete('/delete-order/:id', controller.deleteOrder);
 router.delete('/dellete-all-orders-franchise/:franchise', controller.deleteAllOrdersByFranchise);
 router.delete('/dellete-all-orders', controller.deleteAllOrders);
